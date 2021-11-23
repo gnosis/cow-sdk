d81515962dfea7dad6345c011428d411e0c5de9b
Anxo Rodriguez <anxolin@gmail.com>
2021-11-23 18:54:49 +0000

    Lazy init validator

--- src/utils/appData.ts
+++ src/utils/appData.ts
@@ -1,17 +1,30 @@
 import * as appDataSchema from '../schemas/appData.schema.json'
 import Ajv from 'ajv'
-const ajv = new Ajv()
 
-// let validateDocument 
-const validate = ajv.compile(appDataSchema)
+
+let validate: Ajv.ValidateFunction | undefined
+let ajv: Ajv.Ajv
 
 interface ValidationResult {
   result: boolean,
   errors?: Ajv.ErrorObject[]
 }
 
+function getValidator(): { ajv: Ajv.Ajv, validate: Ajv.ValidateFunction } {
+  if (!ajv) {
+    ajv = new Ajv()  
+  }
+
+  if (!validate) {
+    validate = ajv.compile(appDataSchema)
+  }
+  
+
+  return { ajv, validate }
+}
 
 export async function validateAppDataDocument(appDataDocument: any): Promise<ValidationResult>{
+  const { ajv, validate } = getValidator()
   const result = !!(await validate(appDataDocument))
 
 
