dc34dbb0173da2f87217bd59da8183272fd7383f
Anxo Rodriguez <anxolin@gmail.com>
2021-11-30 16:30:27 +0000

    Add validateAppDataDocument

--- src/Sdk.ts
+++ src/Sdk.ts
@@ -1,8 +1,10 @@
 import { version as SDK_VERSION } from '../package.json'
+import { validateAppDataDocument } from './utils/appData'
 
 export class Sdk {
   static version = SDK_VERSION
 
+  validateAppDataDocument = validateAppDataDocument
 }
 
 export default Sdk
