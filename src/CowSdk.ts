14c9da238066913fdc562d4928864319a24cf67c
Anxo Rodriguez <anxolin@gmail.com>
2021-11-30 17:09:23 +0000

    Rename to CowSDK

--- src/CowSdk.ts
+++ src/CowSdk.ts
@@ -1,10 +1,10 @@
 import { version as SDK_VERSION } from '../package.json'
 import { validateAppDataDocument } from './utils/appData'
 
-export class Sdk {
+export class CowSdk {
   static version = SDK_VERSION
 
   validateAppDataDocument = validateAppDataDocument
 }
 
-export default Sdk
+export default CowSdk
