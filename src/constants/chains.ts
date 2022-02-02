eb09fad22cded9ebee1814d9a26eacd03b26b617
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-02-02 15:44:18 -0300

    add signOrder and signOrderCancellation methods

---
+++ src/constants/chains.ts
@@ -0,0 +1,11 @@
+export enum SupportedChainId {
+  MAINNET = 1,
+  RINKEBY = 4,
+  XDAI = 100,
+}
+
+export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
+  SupportedChainId.MAINNET,
+  SupportedChainId.RINKEBY,
+  SupportedChainId.XDAI,
+]
